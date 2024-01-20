3d47cf27a8761386a62dc8dd51dbf9a77c100787
leokim0711092 <leokim10073@gmail.com>
2024-01-20 15:09:13 +0000

    Robot model

--- tortoisebot_webapp/main.js
+++ tortoisebot_webapp/main.js
@@ -8,6 +8,11 @@ var app = new Vue({
         loading: false,
         rosbridge_address: 'wss://i-077c42467f9b32c8f.robotigniteacademy.com/rosbridge/',
         port: '9090',
+        //robot model
+        viewer: null,
+        tfClient: null,
+        urdfClient: null,
+        //map
         mapViewer: null,
         mapGridClient: null,
         interval: null,
@@ -24,6 +29,11 @@ var app = new Vue({
                 this.connected = true
                 this.loading = false
 
+            // Camera
+                // this.setCamera()
+
+
+            //Map
                 this.mapViewer = new ROS2D.Viewer({
                     divID: 'map',
                     width: 420,
@@ -42,6 +52,9 @@ var app = new Vue({
                     this.mapViewer.shift(this.mapGridClient.currentGrid.pose.position.x, this.mapGridClient.currentGrid.pose.position.y)
                 })
 
+                // Robot model
+                this.setup3DViewer()
+
             })
             this.ros.on('error', (error) => {
                 this.logs.unshift((new Date()).toTimeString() + ` - Error: ${error}`)
@@ -50,12 +63,79 @@ var app = new Vue({
                 this.logs.unshift((new Date()).toTimeString() + ' - Disconnected!')
                 this.connected = false
                 this.loading = false
-                document.getElementById('map').innerHTML = ''
+                //Robot model
+                this.unset3DViewer()
+                //map
+                document.getElementById('map').innerHTML = ''                
+                // document.getElementById('divCamera').innerHTML = ''
+
+                
 
             })
         },
         disconnect: function() {
             this.ros.close()
+        },
+
+        // Robot model method
+         setup3DViewer() {
+            this.viewer = new ROS3D.Viewer({
+                background: '#cccccc',
+                divID: 'div3DViewer',
+                width: 420,
+                height: 360,
+                antialias: true,
+                fixedFrame: 'odom'
+            })
+
+            // Add a grid.
+            this.viewer.addObject(new ROS3D.Grid({
+                color:'#0181c4',
+                cellSize: 0.5,
+                num_cells: 20
+            }))
+
+            // Setup a client to listen to TFs.
+            this.tfClient = new ROSLIB.TFClient({
+                ros: this.ros,
+                angularThres: 0.01,
+                transThres: 0.01,
+                rate: 10.0
+            })
+
+            // Setup the URDF client.
+            this.urdfClient = new ROS3D.UrdfClient({
+                ros: this.ros,
+                param: 'robot_description',
+                tfClient: this.tfClient,
+                // We use "path: location.origin + location.pathname"
+                // instead of "path: window.location.href" to remove query params,
+                // otherwise the assets fail to load
+                path: location.origin + location.pathname,
+                rootObject: this.viewer.scene,
+                loader: ROS3D.COLLADA_LOADER_2
+            })
+        },
+        unset3DViewer() {
+            document.getElementById('div3DViewer').innerHTML = ''
+        },
+        //**Robot model end */
+
+        //Camera
+        setCamera: function() {
+            let without_wss = this.rosbridge_address.split('wss://')[1]
+            console.log(without_wss)
+            let domain = without_wss.split('/')[0] + '/' + without_wss.split('/')[1]
+            console.log(domain)
+            let host = domain + '/cameras'
+            let viewer = new MJPEGCANVAS.Viewer({
+                divID: 'divCamera',
+                host: host,
+                width: 320,
+                height: 240,
+                topic: '/camera/rgb/image_raw',
+                ssl: true,
+            })
         },
     },
     mounted() {
