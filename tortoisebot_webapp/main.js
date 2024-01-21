5f591906098a2e324063a836c07a871345674c34
leokim0711092 <leokim10073@gmail.com>
2024-01-21 07:40:29 +0000

    Joysticks and Action Server

--- tortoisebot_webapp/main.js
+++ tortoisebot_webapp/main.js
@@ -16,6 +16,36 @@ var app = new Vue({
         mapViewer: null,
         mapGridClient: null,
         interval: null,
+         // Joysticks dragging data
+        dragging: false,
+        x: 'no',
+        y: 'no',
+
+        dragCircleStyle: {
+            margin: '0px',
+            top: '0px',
+            left: '0px',
+            display: 'none',
+            width: '75px',
+            height: '75px',
+        },
+
+        // joystick valules
+        joystick: {
+            vertical: 0,
+            horizontal: 0,
+        },
+        // publisher
+        pubInterval: null,
+
+        //Action Server
+        goal: null,
+        action: {
+            goal: { position: {x: 0, y: 0, z: 0} },
+            feedback: { position: 0, state: 'idle' },
+            result: { success: false },
+            status: { status: 0, text: '' },
+        }
     },
     // helper methods to connect to ROS
     methods: {
@@ -55,6 +85,10 @@ var app = new Vue({
                 // Robot model
                 this.setup3DViewer()
 
+                // Joysticks
+                this.pubInterval = setInterval(this.publish, 100)
+
+
             })
             this.ros.on('error', (error) => {
                 this.logs.unshift((new Date()).toTimeString() + ` - Error: ${error}`)
@@ -68,11 +102,65 @@ var app = new Vue({
                 //map
                 document.getElementById('map').innerHTML = ''                
                 document.getElementById('divCamera').innerHTML = ''
-
+                //Joystick
+                clearInterval(this.pubInterval)
                 
-
             })
         },
+
+        // Joystick
+        publish: function() {
+            let topic = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/cmd_vel',
+                messageType: 'geometry_msgs/Twist'
+            })
+            let message = new ROSLIB.Message({
+                linear: { x: this.joystick.vertical, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: this.joystick.horizontal, },
+            })
+            topic.publish(message)
+        },
+
+        startDrag() {
+            this.dragging = true
+            this.x = this.y = 0
+        },
+        stopDrag() {
+            this.dragging = false
+            this.x = this.y = 'no'
+            this.dragCircleStyle.display = 'none'
+            this.resetJoystickVals()
+        },
+        doDrag(event) {
+            if (this.dragging) {
+                this.x = event.offsetX
+                this.y = event.offsetY
+                let ref = document.getElementById('dragstartzone')
+                this.dragCircleStyle.display = 'inline-block'
+
+                let minTop = ref.offsetTop - parseInt(this.dragCircleStyle.height) / 2
+                let maxTop = minTop + 200
+                let top = this.y + minTop
+                this.dragCircleStyle.top = `${top}px`
+
+                let minLeft = ref.offsetLeft - parseInt(this.dragCircleStyle.width) / 2
+                let maxLeft = minLeft + 200
+                let left = this.x + minLeft
+                this.dragCircleStyle.left = `${left}px`
+
+                this.setJoystickVals()
+            }
+        },
+        setJoystickVals() {
+            this.joystick.vertical = -1 * ((this.y / 200) - 0.5)
+            this.joystick.horizontal = -1 * ((this.x / 200) - 0.5)
+        },
+        resetJoystickVals() {
+            this.joystick.vertical = 0
+            this.joystick.horizontal = 0
+        },
+        //** End of Joystick function */
         disconnect: function() {
             this.ros.close()
         },
@@ -131,11 +219,43 @@ var app = new Vue({
             let viewer = new MJPEGCANVAS.Viewer({
                 divID: 'divCamera',
                 host: host,
-                width: 320,
-                height: 240,
+                width: 420,
+                height: 360,
                 topic: '/camera/image_raw',
                 ssl: true,
             })
+        },
+        //Action
+        sendGoal: function() {
+            let actionClient = new ROSLIB.ActionClient({
+                ros : this.ros,
+                serverName : '/tortoisebot_as/',
+                actionName : 'course_web_dev_ros/WaypointActionAction'
+            })
+
+            this.goal = new ROSLIB.Goal({
+                actionClient : actionClient,
+                goalMessage: {
+                    ...this.action.goal
+                }
+            })
+
+            this.goal.on('status', (status) => {
+                this.action.status = status
+            })
+
+            this.goal.on('feedback', (feedback) => {
+                this.action.feedback = feedback
+            })
+
+            this.goal.on('result', (result) => {
+                this.action.result = result
+            })
+
+            this.goal.send()
+        },
+        cancelGoal: function() {
+            this.goal.cancel()
         },
     },
     mounted() {
@@ -145,6 +265,8 @@ var app = new Vue({
                 this.ros.getNodes((data) => { }, (error) => { })
             }
         }, 10000)
-
+        
+        // page is ready
+        window.addEventListener('mouseup', this.stopDrag)
     },
 })
